cfb5bef79b140e879142394e09b0bd92370f0fb4
Dharhas Pothina <dharhas@gmail.com>
2018-05-16 09:16:28 -0500

    add windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,9 @@
+copy whitebox_tools.exe "%LIBRARY_BIN%"
+
+copy %RECIPE_DIR%/setup.py %SRC_DIR%/WBT/
+copy %SRC_DIR%/WBT/LICENSE.txt .
+cd %SRC_DIR%/WBT
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
+if errorlevel 1 exit 1
