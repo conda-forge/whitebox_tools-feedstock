c98e9750ebcbc895f12f3b394228140ffbab2a54
Dharhas Pothina <dharhas@gmail.com>
2018-05-16 16:31:38 -0500

    linux fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    mv WBT/* .
+fi
+
 mkdir -vp ${PREFIX}/bin;
-cp -v WBT/whitebox_tools ${PREFIX}/bin/ || exit 1;
+cp -v whitebox_tools ${PREFIX}/bin/ || exit 1;
 chmod -v 755 ${PREFIX}/bin/whitebox_tools || exit 1;
 
-cp $RECIPE_DIR/setup.py $SRC_DIR/WBT/
-cp $SRC_DIR/WBT/LICENSE.txt .
-cd $SRC_DIR/WBT
+cp $RECIPE_DIR/setup.py $SRC_DIR/
+cd $SRC_DIR
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
