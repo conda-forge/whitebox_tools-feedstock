a10a0970518b1ad646fe709cf8d3bdf0c449d5fa
Dharhas Pothina <dharhas@gmail.com>
2018-05-17 10:56:45 -0500

    fix windows env var

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 copy "%RECIPE_DIR%\setup.py" .
 copy "%SRC_DIR%\whitebox_tools.exe" "%LIBRARY_BIN%"
 
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 
 if errorlevel 1 exit 1
