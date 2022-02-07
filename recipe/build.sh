16afd6bf13c395d01f07aeb773936cff092a21bf
Howard Butler <hobu.inc@gmail.com>
2022-02-06 22:23:54 -0700

    noisy build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
-cargo build --release
+cargo build --release -v
 
 mkdir -vp ${PREFIX}/bin;
-cp -v target/release/whitebox_tools ${PREFIX}/bin/ || exit 1;
-chmod -v 755 ${PREFIX}/bin/whitebox_tools || exit 1;
+cp -v target/release/whitebox-tools-app ${PREFIX}/bin/ || exit 1;
+chmod -v 755 ${PREFIX}/bin/whitebox-tools-app || exit 1;
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
