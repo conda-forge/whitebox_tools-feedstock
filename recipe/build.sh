5a7f3539ed8a02edb04c14923d921dbd70d04461
Howard Butler <hobu.inc@gmail.com>
2022-02-07 08:09:36 -0700

    lint

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 cargo build --release -v
 
 mkdir -vp ${PREFIX}/bin;
+ls -al *
+echo "ls target"
+ls -al target
+echo "ls target/release"
+ls -al ./target/release
+
 cp -v target/release/whitebox-tools-app ${PREFIX}/bin/ || exit 1;
 chmod -v 755 ${PREFIX}/bin/whitebox-tools-app || exit 1;
 
