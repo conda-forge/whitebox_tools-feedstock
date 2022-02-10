d7e090bf992369814643a96b92017fe098d4a926
Howard Butler <hobu.inc@gmail.com>
2022-02-10 10:32:07 -0600

    more rust flailing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 cargo build --release -v
 
-cargo install --root ${PREFIX}
+cargo install --root ${PREFIX} --path .
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
