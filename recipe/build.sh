1a9fe449ac57d9bbd2df04350d73c401578fee07
Howard Butler <hobu.inc@gmail.com>
2022-02-10 10:10:04 -0600

    rust is dumb. why is --root not called --prefix like the rest of the world?

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 cargo build --release -v
 
-cargo install --path ${PREFIX}
+cargo install --root ${PREFIX}
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
