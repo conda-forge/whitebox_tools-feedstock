480b7b0a6f67088e06f7c70f451a4d2e379c9561
Howard Butler <hobu.inc@gmail.com>
2022-02-10 11:11:19 -0600

    list available crates

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 cargo build --release -v
 
-cargo install whitebox-tools-app
+cargo install --list
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
