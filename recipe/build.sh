e835a74492aaf5d9810fba3f99b81e70d37b8b9e
Howard Butler <hobu.inc@gmail.com>
2022-02-10 09:52:56 -0600

    cargo install

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 cargo build --release -v
 
-mkdir -vp ${PREFIX}/bin;
-ls -al target/release/
-ls -al target/release/build/
-ls -al ./target/release/build/whitebox-tools-app/src
-
-cp -v target/release/build/whitebox-tools-app ${PREFIX}/bin/ || exit 1;
-chmod -v 755 ${PREFIX}/bin/whitebox-tools-app || exit 1;
+cargo install --path ${PREFIX}
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
