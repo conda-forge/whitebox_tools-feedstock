567634b9ced539b7a9108b1e6450e6cdf4474fad
Howard Butler <hobu.inc@gmail.com>
2022-02-14 16:39:33 -0600

    install logic

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 cargo build --release -v
 
-cargo install whitebox-lidar
+mkdir -vp ${PREFIX}/bin
+cp -v target/release/whitebox_tools ${PREFIX}/bin/ || exit 1;
+chmod -v 755 ${PREFIX}/bin/whitebox_tools || exit 1;
 
 cp $RECIPE_DIR/setup.py .
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
