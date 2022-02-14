7bd63bbe5e33f5b52998613684f5a52dfbbf1e73
Howard Butler <hobu.inc@gmail.com>
2022-02-14 16:59:29 -0600

    try some more lint

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 cargo build --release -v
 
 mkdir -vp ${PREFIX}/bin
+ls target/release
 cp -v target/release/whitebox_tools ${PREFIX}/bin/ || exit 1;
 chmod -v 755 ${PREFIX}/bin/whitebox_tools || exit 1;
 
