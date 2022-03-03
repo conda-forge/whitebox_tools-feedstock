e7f32db79d4d7fa1616862479b045622eedb19cb
Philip Thiem <pthiem@usgs.gov>
2022-03-03 11:22:54 -0600

    Environment Variable Changes Target

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,14 @@
 cargo build --release -v
 
 mkdir -vp ${PREFIX}/bin
-ls target/release
-cp -v target/release/whitebox_tools ${PREFIX}/bin/ || exit 1;
+if [ ! -z "${CARGO_BUILD_TARGET}" ]; then
+    SOURCE_DIR="target/${CARGO_BUILD_TARGET}/release"
+else
+    SOURCE_DIR=target/release
+fi
+
+ls "${SOURCE_DIR}"
+cp -v "${SOURCE_DIR}/whitebox_tools" ${PREFIX}/bin/ || exit 1;
 chmod -v 755 ${PREFIX}/bin/whitebox_tools || exit 1;
 
 cp $RECIPE_DIR/setup.py .
